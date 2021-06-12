1ba45ae94f7a2271927e11bc5bb34af1b76196a8
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-06-13 02:37:06 +0300

    Add authentication to backend

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['cors']);
+        $this->middleware(['auth:sanctum'])->only(['logout', 'isLoggedIn']);
+        // $this->middleware(['log.routes']);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed',
+        ]);
+
+        $user = User::create([
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Bad credencials'], 401);
+        }
+
+        $user = auth()->user();
+
+        $response = [
+            'user' => $user,
+            'message' => 'The user is registered!'
+        ];
+
+        return response($response);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'The username and password you entered does not match any account! Please try again'], 401);
+        }
+
+        $user = auth()->user();
+
+        $response = [
+            'user' => $user,
+            'message' => 'The user is logged in!'
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response(['message' => 'The user is logged out']);
+    }
+
+    public function isLoggedIn(Request $request)
+    {
+        $user = $request->user();
+
+        $response = [
+            'user' => $user,
+            'message' => 'The user is logged in!',
+            'reloaded_at' => date('c'),
+        ];
+
+        return response($response);
+    }
+}
