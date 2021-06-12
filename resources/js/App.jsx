4e3c6f21c302fe7a59edc3635b7a355f802c4cff
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-06-12 15:30:23 +0300

    Add RouteList to App component

---
+++ resources/js/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import classNames from 'classnames';
+import React from 'react';
+import RouteList from './Components/RouteList';
+
+export default function App() {
+    return (
+        <div className={classNames('App wrapper min-vh-100')}>
+            <RouteList />
+        </div>
+    );
+}
