3fc82702a5b7fb8415a8230c512bc050a299b881
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-06-12 14:57:42 +0300

    Add main React component

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,16 @@
-require('./bootstrap');
+/* eslint-disable react/jsx-filename-extension */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap';
+import React from 'react';
+import { debugContextDevtool } from 'react-context-devtool';
+import { render } from 'react-dom';
+import App from './App';
+
+const container = document.getElementById('app');
+
+render(
+    <App />,
+    container,
+);
+
+debugContextDevtool(container);
