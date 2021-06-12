ac49cd18dd3ac5cdbb92d837dd6aad1bfad43e78
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-06-12 14:19:21 +0300

    Add main, login, logout and register routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
+Route::view('/{path?}', 'app')->where('path', '(.*)')->name('default');
