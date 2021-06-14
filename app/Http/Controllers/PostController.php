1e203649b42dffdd8e662b0e399ef82ea8fe9318
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-06-14 05:26:10 +0300

    Add controllers

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        return Post::select([
+            '*',
+            'username' => User::selectRaw('username')->whereColumn('id', 'user_id'),
+            'avatar' => User::selectRaw('avatar')->whereColumn('id', 'user_id'),
+        ])->withCount(['comments'])->get();
+        // return  Post::all()->load('user');
+        // return  Post::with('user');
+    }
+
+     /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validation = $request->validate([
+            'title' => 'required|max:255|unique:posts',
+            'content' => 'required',
+            'user_id' => 'required|integer',
+        ]);
+
+        Post::create($request->all());
+
+        return $validation;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return Post::find($post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
