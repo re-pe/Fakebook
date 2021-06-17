dc2fa84c0dfabe9a3a30b3c2d08362c4ffe090d9
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-06-18 02:04:46 +0300

    Add names to routes

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\AuthController;
 */
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::view('/{path?}', 'app')->where('path', '(.*)')->name('default');
