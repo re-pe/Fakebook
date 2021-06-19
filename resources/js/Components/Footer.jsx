bd86218e0a771085607caf277e52ea05885229d2
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-06-19 08:59:02 +0300

    Update footer

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
+import '../../css/Footer.css';
 
 export default function Footer() {
     return (
@@ -14,12 +15,14 @@ export default function Footer() {
                 variant="dark"
                 sticky="bottom"
                 className="navbar text-light shadow-sm p-3 m-0 mt-5"
+                // eslint-disable-next-line no-alert
+                onSelect={() => alert('redas(dot)peskaitis(at)gmail(dot).lt')}
                 expand
             >
                 <Nav className="text-center mx-auto" navbar>
-                    <span>
-                        &copy; 2021 Rėdas Peškaitis
-                    </span>
+                    <Nav.Item>
+                        <Nav.Link className="text-white" eventKey="x">&copy; 2021 Rėdas Peškaitis</Nav.Link>
+                    </Nav.Item>
                 </Nav>
             </Navbar>
         </Container>
